a6a9d6521c1c928dd98d6374432a3bcaffad26e8
ohmrefresh <ohmrefresh@gmail.com>
2016-06-06 11:49:49 +0700

    add kryo to improve performance -Serializers -Compression and encryption(Serializers object)

--- app/src/main/java/ark/wekaport/MainActivity.java
+++ app/src/main/java/ark/wekaport/MainActivity.java
@@ -4,9 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.zip.DeflaterInputStream;
+import java.util.zip.DeflaterOutputStream;
+import weka.classifiers.Classifier;
 import weka.classifiers.evaluation.Evaluation;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
@@ -45,9 +54,65 @@ public class MainActivity extends AppCompatActivity {
       Log.e("Result", "================================================");
 
       Toast.makeText(this, result, Toast.LENGTH_LONG).show();
+
     } catch (Exception e) {
 
       e.printStackTrace();
+    }
+
+
+
+  }
+
+
+  /**
+   * Write Model To file
+   * https://github.com/EsotericSoftware/kryo
+   * @param m_Classifier
+   */
+  public void writeModel(Classifier m_Classifier){
+    try {
+
+
+      //Kryo kryo = new Kryo();
+      //Output output = new Output(new FileOutputStream("file.bin"));
+      //kryo.writeObject(output, m_Classifier);
+      //output.close();
+
+
+      OutputStream outputStream = new DeflaterOutputStream(new FileOutputStream("file.bin"));
+      Output output = new Output(outputStream);
+      Kryo kryo = new Kryo();
+      kryo.writeObject(output, m_Classifier);
+      output.close();
+
+    }catch (Exception e){
+
+    }
+  }
+
+  /**
+   * Read training Object
+   * https://github.com/EsotericSoftware/kryo
+   * @return
+   */
+  public Classifier readModel(){
+    try {
+      Kryo kryo = new Kryo();
+      //Input input = new Input(new FileInputStream("file.bin"));
+      //
+      //Classifier obj = kryo.readObject(input, Classifier.class);
+      //input.close();
+
+      InputStream input = new DeflaterInputStream(new FileInputStream("file.bin"));
+      Classifier obj = kryo.readObject((Input) input, Classifier.class);
+      input.close();
+
+
+      return  obj;
+    }catch (Exception e){
+
+      return null;
     }
   }
 }
